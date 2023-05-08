a90d199dc0bb275c0286ecc6f0ef3e43efb8c6d1
JZITNIK-config <kintiz@seznam.cz>
2023-05-08 15:29:05 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+// index.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+app.use(express.json())
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get("/api/:date",function (req,res) {
+  const date = new Date(req.params.date)
+  res.json({unix:date.getTime(),utc:date.toUTCString()})
+})
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+
+// listen for requests :)
+var listener = app.listen(8001, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
